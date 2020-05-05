b5276a89c51918838e96f37daa9cf8185c7eb1ba
sankeerna korla <sankeerna.reddy14@gmail.com>
2020-05-04 22:55:08 -0700

    Add new name to list of scientists
    - Also introduce ember-bootstrap
    -Introduce ember decorators
    -Add new classes file _sizing.scss

--- app/components/people-list.js
+++ app/components/people-list.js
@@ -1,9 +1,22 @@
+/* eslint-disable no-console */
 import Component from '@ember/component';
+import { action, set } from '@ember/object';
+import { reads } from '@ember/object/computed';
 
-export default Component.extend({
-	actions: {
-				showPerson(person) {
-				alert(person);
-				}
-			}
-});
+
+
+export default class PeopleList extends Component {
+
+	@reads('people')
+	scientists;
+
+
+	@action
+	showPeople() {
+		const { personName, scientists } = this;
+		scientists.push(personName);
+		set(this, 'scientists', scientists);
+		set(this, 'personName', '');
+		console.log('scientists', scientists);
+	}
+}
